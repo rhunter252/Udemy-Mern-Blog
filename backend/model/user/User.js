78e9e4d7b0d16953aaaf627641632fd9e37fd759
Randy Hunter <randyh252@gmail.com>
2023-02-20 05:27:39 -0500

    updated user controller and user routes

--- backend/model/user/User.js
+++ backend/model/user/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 
 //create schema
 const userSchema = new mongoose.Schema(
@@ -100,10 +101,25 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+//virtual method to populate created post
+userSchema.virtual("posts", {
+  ref: "Post",
+  foreignField: "user",
+  localField: "_id",
+});
 
-// Hash password
+//Account Type
+userSchema.virtual("accountType").get(function () {
+  const totalFollowers = this.followers?.length;
+  return totalFollowers >= 1 ? "Pro Account" : "Starter Account";
+});
+
+//Hash password
 userSchema.pre("save", async function (next) {
-  // hash password
+  if (!this.isModified("password")) {
+    next();
+  }
+  //hash password
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
@@ -112,6 +128,30 @@ userSchema.pre("save", async function (next) {
 //match password
 userSchema.methods.isPasswordMatched = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//Verify account
+userSchema.methods.createAccountVerificationToken = async function () {
+  //create a token
+  const verificationToken = crypto.randomBytes(32).toString("hex");
+  this.accountVerificationToken = crypto
+    .createHash("sha256")
+    .update(verificationToken)
+    .digest("hex");
+  this.accountVerificationTokenExpires = Date.now() + 30 * 60 * 1000; //10 minutes
+  return verificationToken;
+};
+
+//Password reset/forget
+
+userSchema.methods.createPasswordResetToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000; //10 minutes
+  return resetToken;
 };
 
 //Compile schema into model
--- frontend/mern-blog-frontend/src/App.js
+++ frontend/mern-blog-frontend/src/App.js
@@ -1,12 +1,13 @@
-import Homepage from "./components/Homepage/Homepage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/Homepage/Homepage";
+import Register from "./components/Users/Register/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
